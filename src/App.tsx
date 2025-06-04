c6f004bee2cc8a22eee5cd75e7ab9f6ef2d3c3c3
Romain Verduci <rverduci@cloudbees.com>
2025-06-04 08:24:16 +0200

    Remove feature flag: showMessage

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {LoadingIndicator} from "./LoadingIndicator.tsx";
 
 function App() {
 
-  const featureFlags = useFeatureFlags()
+  const featureFlags = useFeatureFlags();
 
   if (featureFlags.loading) {
     return (
@@ -19,11 +19,9 @@ function App() {
     <>
       <h1>CloudBees feature management React sample application</h1>
       <div className="card">
-        {featureFlags.showMessage.isEnabled() && (
-          <p style={{color: featureFlags.fontColor.getValue(), fontSize: featureFlags.fontSize.getValue()}}>
-            {featureFlags.message.getValue()}
-          </p>
-        )}
+        <p style={{color: featureFlags.fontColor.getValue(), fontSize: featureFlags.fontSize.getValue()}}>
+          {featureFlags.message.getValue()}
+        </p>
       </div>
 
       <div className="card">
--- src/feature-management/flags.ts
+++ src/feature-management/flags.ts
@@ -7,8 +7,6 @@ export interface IFeatureFlagsState extends IFeatureFlags {
 }
 
 export const flags = {
-  // Boolean - should the message be shown?
-  showMessage: new Flag(),
   // String - the message to show.
   message: new RoxString('This is the default message; try changing some flag values!'),
   // String (with options) - the color of the message text.
