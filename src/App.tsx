6cb8a8ce3f8ef52a80ff4824c253472a7e13a06d
Romain Verduci <rverduci@cloudbees.com>
2025-06-04 11:22:24 +0200

    Remove feature flag: showMessage

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
     <>
       <h1>CloudBees feature management React sample application</h1>
       <div className="card">
-        {featureFlags.showMessage.isEnabled() && (
+        {true /* featureFlags.showMessage.isEnabled() */ && (
           <p
             style={{
               color: featureFlags.fontColor.getValue(),
