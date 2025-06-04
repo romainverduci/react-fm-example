89a877d5446a8a072293ea22d12a77767eb74393
Romain Verduci <rverduci@cloudbees.com>
2025-06-04 08:10:40 +0200

    Remove feature flag: showMessage

--- src/LoadingIndicator.tsx
+++ src/LoadingIndicator.tsx
@@ -1,11 +1,9 @@
 export const LoadingIndicator = () => {
-  const showMessage = true; // This can be set based on your application's state
-
   return (
-   { showMessage ? <div className="position-absolute top-50 start-50 translate-middle">
+    <div className="position-absolute top-50 start-50 translate-middle">
       <div className="spinner-border text-primary" role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
-    </div> : null }
+    </div>
   )
 }
